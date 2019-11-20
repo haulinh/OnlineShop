051af34dde62526ddca8db5cb8e231ff5a518859
nguyenngocnhatminh <37469058+nguyenngocnhatminh@users.noreply.github.com>
2019-11-20 19:51:56 +0700

    contactDao,AboutDao

---
+++ Src/OnlineShop/Model/Dao/AboutDao.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EntityFramework;
+using Model.ViewModel;
+using PagedList;
+namespace Model.Dao
+{
+    public class AboutDao
+    {
+        OnlineShopDbContext db = null;
+        public AboutDao()
+        {
+            db = new OnlineShopDbContext();
+        }
+        public IEnumerable<About> ListAllPaging(int page, int pageSize)
+        {
+            IOrderedQueryable<About> Abouts = db.Abouts;
+            return Abouts.OrderByDescending(x => x.ID).ToPagedList(page, pageSize);
+        }
+
+        public About AboutDetail(int ID)
+        {
+            return db.Abouts.Find(ID);
+        }
+
+        public long Insert(About entity)
+        {
+            entity.CreatedDate = DateTime.Now;
+            //entity.CreatedBy = nguoi dang nhap hien tai ; 
+            db.Abouts.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        public bool Update(About entity)
+        {
+            try
+            {
+                var About = db.Abouts.Find(entity.ID);
+                About.Name = entity.Name;
+                About.MetaTitle = entity.MetaTitle;
+                About.Description = entity.Description;
+                About.Image = entity.Image;
+                About.Detail = entity.Detail;
+                About.ModifiedDate = DateTime.Now;
+                //About.ModifiedBy = nguoidangnhaphientai
+                About.MetaDescriptions = entity.MetaDescriptions;
+                About.MetaKeywords = entity.MetaKeywords;
+                About.Status = entity.Status;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Src/OnlineShop/Model/Dao/ContactDao.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EntityFramework;
+using Model.ViewModel;
+using PagedList;
+namespace Model.Dao
+{
+    public class ContactDao
+    {
+        OnlineShopDbContext db = null;
+        public ContactDao()
+        {
+            db = new OnlineShopDbContext();
+        }
+        public IEnumerable<Contact> ListAllPaging(int page, int pageSize)
+        {
+            IOrderedQueryable<Contact> contacts = db.Contacts;
+            return contacts.OrderByDescending(x => x.ID).ToPagedList(page, pageSize);
+        }
+
+        public Contact ContactDetail(int ID)
+        {
+            return db.Contacts.Find(ID);
+        }
+
+        public int Insert(Contact entity)
+        {
+            db.Contacts.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        public bool Update(Contact entity)
+        {
+            try
+            {
+                var contact = db.Contacts.Find(entity.ID);
+                contact.Content = entity.Content;
+                contact.Status = entity.Status;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
