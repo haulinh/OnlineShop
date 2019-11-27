e468aa87116f69285d9331d05bfcee5b34721af9
nguyenngocnhatminh <37469058+nguyenngocnhatminh@users.noreply.github.com>
2019-11-28 05:01:59 +0700

    Ql content
    chưa hiển thị tất cả các tag của content

---
+++ Src/OnlineShop/OnlineShop/Areas/Admin/Controllers/ContentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model.Dao;
+using Model.EntityFramework;
+using System.Threading.Tasks;
+using OnlineShop.Common;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class ContentController : Controller
+    {
+        // GET: Admin/Content
+        public ActionResult Index()
+        {
+            var dao = new ContentDao();
+            var listContent = dao.GetListContent();
+            return View(listContent);
+        }
+    }
+}
