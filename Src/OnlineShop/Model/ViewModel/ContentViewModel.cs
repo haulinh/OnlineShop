d0035ffe70fb306fd85bdebf662f615d645a2b23
nguyenngocnhatminh <37469058+nguyenngocnhatminh@users.noreply.github.com>
2019-11-28 19:17:05 +0700

    contentview model

---
+++ Src/OnlineShop/Model/ViewModel/ContentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Model.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Model.ViewModel
+{
+    public class ContentViewModel
+    {
+        public Content content;
+        public List<Tag> tag;
+
+    }
+}
